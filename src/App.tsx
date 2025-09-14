ed5a384c1a88dee021616cc1de8513329be7e770
2phonesavanh <phonesavanh.csp@gmail.com>
2025-09-14 23:43:01 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import AppRouter from "./routes";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/*" element={<AppRouter />} />
-      </Routes>
+      <AppRouter />
   );
 }
 
